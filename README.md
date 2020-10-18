8882f1f9a6db3ed904c076638a8e21844a972227
lework <lework@yeah.net>
2020-10-18 18:55:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# kainstall-offline
+
+[kainstall](https://github.com/lework/kainstall) 安装程序的离线包
+
+
+
+## 文件列表
+
+| kube 版本 | 文件大小 | 下载链接 |
+| --------- | -------- | ----------- |
+|           |          |             |
+
+
+
+## 文件内容
+
+> 只含有 kainstall 默认使用的文件。
+
+### rpms
+
+| 用途    | 包名                                                         |
+| ------- | ------------------------------------------------------------ |
+| all     | sshpass openssh wget gzip ipvsadm ipset sysstat conntrack libseccomp unzip epel-release chrony bash-completion docker-ce docker-ce-cli containerd.io |
+| kubeadm | kubeadm kubelet  kubectl                                     |
+| worker  | haproxy                                                      |
+| kernel  | kernel-ml kernel-devel                                       |
+
+### images
+
+| 用途   | 镜像                                                         |
+| ------ | ------------------------------------------------------------ |
+| all    | kube-proxy flannel pause                                     |
+| master | etcd kube-scheduler kube-controller-manager kube-apiserver coredns |
+| worker | metrics-server metrics-scraper kubernetesui dashboard kube-webhook-certgen whoami traefik ingress-nginx-controller defaultbackend |
+
+### manifests
+
+- kube-flannel
+- metrics-server
+- ingress-nginx-controller
+- kubernetes-dashboard
+
+
+## License
+
+MIT
---
+++ scripts/archive.sh
@@ -0,0 +1,44 @@
+#!/bin/env bash
+
+KUBE_VERSION=${1:-1.19.2}
+
+mkdir -p /data/{all,kubeadm,worker,kernel}
+cd /data
+ls -alhR ./*
+
+yum install -y epel-release yum-utils
+
+echo "[download kernel package]"
+ver=$(rpm --eval '%{centos_ver}')
+curl https://www.elrepo.org/elrepo-release-${ver}.el${ver}.elrepo.noarch.rpm -o /data/kernel/elrepo-release-${ver}.el${ver}.elrepo.noarch.rpm
+yum localinstall -y kernel/elrepo-release-${ver}.el${ver}.elrepo.noarch.rpm
+yum install -y --downloadonly --downloaddir=kernel --enablerepo=elrepo-kernel kernel-ml kernel-devel
+ls -alhR kernel/*
+
+echo "[download common node package]"
+yum install -y --downloadonly --downloaddir=all sshpass openssh openssl wget gzip ipvsadm ipset sysstat conntrack libseccomp unzip epel-release chrony bash-completion
+yum reinstall -y --downloadonly --downloaddir=all gzip libselinux libseccomp systemd systemd-libs 
+
+echo "[download docker package]"
+yum-config-manager --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y --downloadonly --downloaddir=all docker-ce docker-ce-cli containerd.io
+ls -alhR all/*
+
+echo "[download kubeadm package]"
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+yum install -y --downloadonly --downloaddir=kubeadm --disableexcludes=kubernetes kubeadm-${KUBE_VERSION} kubelet-${KUBE_VERSION} kubectl-${KUBE_VERSION}
+ls -alhR kubeadm/*
+
+echo "[download worker node package]"
+yum install -y --downloadonly --downloaddir=worker haproxy
+ls -alhR worker/*
